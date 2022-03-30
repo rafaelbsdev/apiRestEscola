d4bcd1bd83dd230423ee41e9b1ab4d02eb7c6fd1
rafaelbs <rafaelbs.dev@gmail.com>
2022-03-30 16:39:06 -0300

    add cors and helmet

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import { resolve } from 'path';
 dotenv.config();
 
 import './database';
+// import cors from 'cors';
+// import helmet from 'helmet';
 
 import express from 'express';
 import homeRoutes from './routes/homeRoutes';
@@ -11,6 +13,21 @@ import userRoutes from './routes/userRoutes';
 import tokenRoutes from './routes/tokenRoutes';
 import alunoRoutes from './routes/alunoRoutes';
 import fotoRoutes from './routes/fotoRoutes';
+
+// const whiteList = [
+//   'http://react2.otaviomiranda.com.br',
+//   'http://localhost:3001',
+// ];
+
+// const corsOptions = {
+//   origin(origin, callback) {
+//     if (whiteList.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+// };
 
 class App {
   constructor() {
@@ -20,6 +37,8 @@ class App {
   }
 
   middlewares() {
+    // this.app.use(cors(corsOptions));
+    // this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use(express.static(resolve(__dirname, 'uploads')));
